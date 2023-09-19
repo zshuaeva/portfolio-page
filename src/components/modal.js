b1af9e5aa45c7de09198f8d98cc5ab1dca6272f0
Joshua <jpe7619@gmail.com>
2023-09-19 13:34:03 -0700

    integrate modal component for project screenshot

---
+++ src/components/modal.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+} from '@chakra-ui/react';
+import '../stylesheets/modal.css';
+import { Link } from 'react-router-dom';
+
+export default function PhotoModal({ imageSrc }) {
+  const OverlayOne = () => (
+    <ModalOverlay
+      bg="blackAlpha.300"
+      backdropFilter="blur(10px) hue-rotate(90deg)"
+    />
+  );
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [overlay, setOverlay] = React.useState(<OverlayOne />);
+
+  return (
+    <>
+      <Link
+        className="photo-button"
+        onClick={() => {
+          setOverlay(<OverlayOne />);
+          onOpen();
+        }}
+      >
+        View Photo
+      </Link>
+
+      <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        {overlay}
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody className="modal-photo">
+            <img src={imageSrc} alt="Project" />
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
